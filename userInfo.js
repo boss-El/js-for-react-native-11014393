0cd35a45ada61db6abca5276a74cc6a6eef5ca7b
boss-EL <edatsomor010@st.ug.edu.gh>
2024-05-19 11:02:42 +0000

    added createUserProfiles function

---
+++ userInfo.js
@@ -0,0 +1,9 @@
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        id: index + 1,
+        originalName: name,
+        modifiedName: modifiedNames[index]
+    }));
+}
+
+module.exports = { createUserProfiles };
