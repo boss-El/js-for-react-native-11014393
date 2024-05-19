25cb73b2a8e833172724e4e204a4e496fa1f0c2f
boss-EL <edatsomor010@st.ug.edu.gh>
2024-05-19 11:25:23 +0000

    final changes

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,9 +1,8 @@
+// arrayManipulation.js
 
 function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num * num : num * 3);
 }
-
-module.exports = { processArray };
 
 function formatArrayStrings(strings, numbers) {
     return strings.map((str, index) => {
